42c8849bedf0fc4c43eba513903f399df4ca9030
Matthew Kusnadi <matthew.kusnadi@hotmail.com>
2023-10-06 13:08:30 +1100

    edit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
 import './App.css';
 import { auth } from './firebase.init';
+import { createUserWithEmailAndPassword,
+signInWithEmailAndPassword, } from "firebase/auth";
+
 
 function App() {
   function register(){
     console.log('register');
+    createUserWithEmailAndPassword(auth, 'email@email.com', 'test123')
+    .then((user)=>{
+      console.log(user)
+    })
+    .catch((error)=>{
+      console.log(error)
+    })
+  }
+
+  function login(){
+    signInWithEmailAndPassword (auth, 'email@email.com', 'test123')
+    .then((user)=>{
+      console.log(user)
+    })
+    .catch((error)=>{
+      console.log(error)
+    })
   }
   return (
     <div className="App">
       <button onClick={register}>Register</button>
+      <button onClick={login}>Login</button>
     </div>
   );
 }
